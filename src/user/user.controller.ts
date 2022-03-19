79049a0dbd51756141b2948b792c9dffb75b824e
BawNer <aleksandr.krasnikov2000@gmail.com>
2022-03-19 12:52:14 +0300

    add route and auth guard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,9 @@
-import { Body, Controller, Get, Param, Post, UsePipes, ValidationPipe } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Put, UseGuards, UsePipes, ValidationPipe } from "@nestjs/common";
+import { User } from "./decorators/user.decorator";
 import { CreateUserDto } from "./dto/createUser.dto";
+import { LoginUserDto } from "./dto/loginUser.dto";
+import { UpdateUserDto } from "./dto/updateUser.dto";
+import { AuthGuard } from "./guards/auth.guard";
 import { UserResponseInteface } from "./types/responseUser.inteface";
 import { UserEntity } from "./user.entity";
 import UserService from "./user.service";
@@ -10,15 +14,33 @@ export default class UserController {
     private readonly userServise: UserService
   ) {}
 
-  @Get(':code/activation')
-  async getActiovationLink(@Param('code') code: string): Promise<string> {
-    return await this.userServise.generateActivationLink(code)
+  @Get('activation')
+  @UseGuards(AuthGuard)
+  async getActiovationLink(@User('id') id: number): Promise<string> {
+    return await this.userServise.generateActivationLink(id)
   }
+
+  // @Get('activation/:code') activate email service here
+
 
   @Post('registration')
   @UsePipes(new ValidationPipe())
   async createUser(@Body('user') createUserDto: CreateUserDto): Promise<UserResponseInteface> {
     const user = await this.userServise.createUser(createUserDto)
     return this.userServise.buildResponse(user)
+  }
+
+  @Post('login')
+  @UsePipes(new ValidationPipe())
+  async loginUser(@Body('user') loginUserDto: LoginUserDto): Promise<UserResponseInteface> {
+    const user = await this.userServise.loginUser(loginUserDto)
+    return this.userServise.buildResponse(user)
+  }
+
+  @Put()
+  @UseGuards(AuthGuard)
+  @UsePipes(new ValidationPipe())
+  async updateUser(@User('id') id: number, @Body('user') updateUserDto: UpdateUserDto): Promise<UserEntity> {
+    return await this.userServise.updateUser(updateUserDto, id)
   }
 }
